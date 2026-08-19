c48d233def13a78d3b17197da6b8abb6cbf69c46
Brett Gentile <bgentile19@gmail.com>
2026-08-19 17:24:52 -0400

    Build Game class models

---
+++ MeepleLedger/Domain/Game.cs
@@ -0,0 +1,26 @@
+﻿namespace MeepleLedger.Domain
+{
+    public class Game
+    {
+        public required string Name;
+        int MinPlayers;
+        int MaxPlayers;
+        int PlayTimeMinutes;
+    }
+
+    public class OwnedGame
+    {
+        public required Game Game;
+        public DateTime DateAcquired;
+        public Condition Condition;
+        public string Notes;
+    }
+
+    public enum Condition
+    {
+        Mint,
+        Good,
+        Played,
+        Worn,
+    }
+}
